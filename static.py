571c7dc94051aaea73ae2155b5fbc1ab9f17d79b
Al-ImranRony <imranrony687@gmail.com>
2021-05-20 01:32:11 +0600

    All about class variables and method along with static method.

--- static.py
+++ static.py
@@ -1,32 +1,53 @@
 
 class Employee:
     dept = "SD"                                     # Class/static variable
+    count = 0
 
     def __init__(self, name, id):
         self.name = name
         self.id = id
+        Employee.count += 1
 
-    @staticmethod
-    def salary(amount):
-        return amount
+    @classmethod                                    # class method
+    def department(cls):                            # class method can only access class variables because of no self arg.
+        return cls.dept                             # cls is the short form of Class which is a convention
 
+    @classmethod
+    def show_count(cls):
+        print("Total employees: ", cls.count)
+
+    @staticmethod                                   # Static method most often works with logic neither use class variables nor instance variables
+    def status(id):
+        print("Valid Employee") if (id > 500) else print("Employee not valid !")
+            
+        
+
+
+Employee.show_count()
 
 emp1 = Employee("Ron", 531)
 emp2 = Employee("Jon", 530)
+
+Employee.show_count()
 
 print(emp1.dept)
 print(emp2.dept)
 print(emp2.name)
 print(emp1.id)
 
-print(Employee.dept)                                # Accessing static variable using the class name
+print(Employee.dept)                                # Accessing class variable using the class name
+
+
+# Observe the id's of the variable, they will be similar to each others
+print("ID's of class variable: ", id(Employee.dept), id(emp1.dept), id(emp2.dept)) 
 
 emp1.dept = "SQA"
 print(emp1.dept, emp2.dept)
 
-Employee.dept = "ML"                                # Observe, how static variable can be accessed
+Employee.dept = "ML"                                # Observe, how class variable can be accessed
 print(Employee.dept, emp1.dept, emp2.dept)
 
+print(Employee.department())                        # Accessing class method with class
 
-print(Employee.salary(0000))                        # Accessing static method
-print(emp1.salary(50000))
+Employee.show_count()
+Employee.status(531)                                # Accessing static method with class
